b7689c4c24c02beb6523852c4e7e188282939d8f
sh98923 <sh98923@naver.com>
2025-04-28 12:18:53 +0900

    0428

--- Assets/Scripts/Character/Character.cs
+++ Assets/Scripts/Character/Character.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.TextCore.Text;
 using static UnityEngine.GraphicsBuffer;
@@ -10,16 +10,28 @@ public class Character : MonoBehaviour
         Idle, Move, Attack, Dead
     }
 
+    private CharacterData _characterData;
     private GameObject _character;
     private Animator _animator;
-    [SerializeField] private float _moveSpeed = 1.0f;
+    private Transform _target;
+    private Collider _collider;
+
+    [SerializeField]
+    private int _moveSpeed = 3;
+
+    private int _enemyLayerMask;
 
     private State _curState;
 
     private void Awake()
     {
+        DataManager.Instance.LoadData();
+
         _character = GetComponent<GameObject>();
         _animator = GetComponentInChildren<Animator>();
+        _collider = GetComponent<Collider>();
+
+        _enemyLayerMask = LayerMask.GetMask("Enemy");
 
     }
 
@@ -30,6 +42,7 @@ public class Character : MonoBehaviour
             case State.Idle:
                 break;
             case State.Move:
+                MoveToTarget();
                 break;
             case State.Attack:
                 break;
@@ -42,27 +55,54 @@ public class Character : MonoBehaviour
     {
         _curState = State.Idle;
         Debug.Log(_curState);
-        _animator.Play("IDLE");
     }
-
-    public void SetMove()
+    public void StartBattle()
     {
+        if (_curState != State.Idle) return;
+
         _curState = State.Move;
         Debug.Log(_curState);
-        _animator.Play("MOVE");
+    }
+    private void SetTarget()
+    {
+        Collider[] colliders = Physics.OverlapSphere(transform.position, _characterData.Range, _enemyLayerMask);
+
+        float minDistance = float.MaxValue;
+
+        _target = null;
+
+        foreach(Collider collider in colliders)
+        {
+            float distance = Vector2.Distance(transform.position, collider.transform.position);
+
+            if(distance < minDistance)
+            {
+                minDistance = distance;
+                _target = collider.transform;
+            }
+        }
     }
 
-    //private void MoveToTarget()
-    //{
-    //    if (!_target)
-    //    {
-    //        Debug.Log("targeterror");
-    //        return;
-    //    }
-    //
-    //    Vector3 direction = _target.position - transform.position;
-    //    float distance = direction.magnitude;
-    //
-    //    transform.Translate(direction.normalized * _moveSpeed * Time.deltaTime);
-    //}
+    private void MoveToTarget()
+    {
+        //if (!_target)
+        //{
+        //    Debug.Log("targeterror");
+        //    return;
+        //}
+        //
+        //Vector3 direction = _target.position - transform.position;
+        //float distance = direction.magnitude;
+        //
+        //transform.Translate(direction.normalized * _moveSpeed * Time.deltaTime);
+
+        SetTarget();
+
+        if (_target == null) return;
+
+        Vector3 direction = _target.position - transform.position;
+        float distance = direction.magnitude;
+
+        transform.Translate(direction.normalized * _moveSpeed * Time.deltaTime);
+    }
 }
---
+++ Assets/Scripts/DataManager.cs
@@ -0,0 +1,56 @@
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+using System.Collections.Generic;
+
+public struct CharacterData
+{
+    public int Key;
+    public string Name;
+    public int PositionX;
+    public int Hp;
+    public int Atk;
+    public float CriRate;
+    public float Range;
+}
+
+public class DataManager : Singleton<DataManager>
+{
+    private Dictionary<int, CharacterData> characterDatas = new Dictionary<int, CharacterData>();
+
+    public CharacterData GetCharacterData(int key)
+    {
+        return characterDatas[key];
+    }
+    public void LoadData()
+    {
+        LoadCharacterTable();
+    }
+
+    private void LoadCharacterTable()
+    {
+        TextAsset textAsset = Resources.Load<TextAsset>("Tables/PlayableTable");
+
+        string temp = textAsset.text.Replace("\r\n", "\n");
+
+        string[] str = temp.Split('\n');
+
+        for (int i = 1; i < str.Length; i++)
+        {
+            if (str[i].Length == 0)
+                return;
+
+            string[] data = str[i].Split(',');
+
+            CharacterData characterData;
+            characterData.Key = int.Parse(data[0]);
+            characterData.Name = data[1];
+            characterData.PositionX = int.Parse(data[2]);
+            characterData.Hp = int.Parse(data[3]);
+            characterData.Atk = int.Parse(data[4]);
+            characterData.CriRate = float.Parse(data[5]);
+            characterData.Range = float.Parse(data[6]);
+
+            characterDatas.Add(characterData.Key, characterData);
+        }
+    }
+}
